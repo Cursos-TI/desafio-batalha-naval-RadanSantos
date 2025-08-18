101f087700487f0eb0ca219dd7aaf78b7cad80b6
Radan Santos <radansantos@gmail.com>
2025-08-18 02:05:31 +0000

    batalha naval 2

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,38 +1,37 @@
 #include <stdio.h>
 
 #define TAM 10
-#define NAVIO 3
-#define AGUA 0
-#define TAM_NAVIO 3
+#define TAM_NAVIO 4
+#define AGUA '~'
+#define NAVIO 'N'
 
-// Inicializa o tabuleiro com 0 (água) usando recursão
-void inicializarTabuleiro(int tabuleiro[TAM][TAM], int i, int j) {
-    if (i >= TAM) return;
+// Inicializar o tabuleiro recursivamente
+void inicializarTabuleiro(int tabuleiro[TAM][TAM], int linha, int coluna) {
+    if (linha >= TAM) return;
 
-    tabuleiro[i][j] = AGUA;
+    tabuleiro[linha][coluna] = AGUA;
 
-    if (j + 1 < TAM) {
-        inicializarTabuleiro(tabuleiro, i, j + 1);
-    } else {
-        inicializarTabuleiro(tabuleiro, i + 1, 0);
-    }
+    if (coluna ==TAM -1)
+        inicializarTabuleiro(tabuleiro, linha + 1, 0);
+     else 
+        inicializarTabuleiro(tabuleiro, linha, coluna + 1);
 }
 
-// Exibe o tabuleiro usando recursão
-void exibirTabuleiro(int tabuleiro[TAM][TAM], int i, int j) {
-    if (i >= TAM) return;
+// Exibir tabuleiro recursivamente
+void exibirTabuleiro(int tabuleiro[TAM][TAM], int linha, int coluna) {
+    if (linha == TAM) return;
 
-    printf("%d ", tabuleiro[i][j]);
+    printf("%c ", tabuleiro[linha][coluna]);
 
-    if (j + 1 < TAM) {
-        exibirTabuleiro(tabuleiro, i, j + 1);
+    if (coluna == TAM - 1) {
+        exibirTabuleiro(tabuleiro, linha + 1, 0);
     } else {
         printf("\n");
-        exibirTabuleiro(tabuleiro, i + 1, 0);
+        exibirTabuleiro(tabuleiro, linha, coluna + 1);
         }
 }
 
-// Posicionar navio horizontalmente com recursão
+// Posicionar navio horizontalmente 
 void posicionarNavioHorizontal(int tabuleiro[TAM][TAM], int linha, int coluna, int tamanho) {
     if (tamanho == 0) return;
 
@@ -43,49 +42,53 @@ void posicionarNavioHorizontal(int tabuleiro[TAM][TAM], int linha, int coluna, i
     }
 }
 
-// Posicionar navio verticalmente com recursão
+// Posicionar navio verticalmente
 void posicionarNavioVertical(int tabuleiro[TAM][TAM], int linha, int coluna, int tamanho) {
     if (tamanho == 0) return;
 
-    // Só posicionar se não ultrapassar os limites
     if (linha < TAM && coluna < TAM) {
         tabuleiro[linha][coluna] = NAVIO;
         posicionarNavioVertical(tabuleiro, linha + 1, coluna, tamanho - 1);
     }
 }
 
+// Posicionar navio na diagonal principal(↘)
+void posicionarNavioDiagonalPrincipal(int tabuleiro[TAM][TAM], int linha, int coluna, int tamanho) {
+    if (tamanho == 0) return;
+
+    if (linha , TAM && coluna < TAM) {
+        tabuleiro[linha][coluna] = NAVIO;
+        posicionarNavioDiagonalPrincipal(tabuleiro, linha + 1, coluna + 1, tamanho -1);
+    }
+}
+
+// Posicionar navio na diagonal secundária (↙)
+void posicionarNavioDiagonalSecundaria(int tabuleiro[TAM][TAM], int linha, int coluna, int tamanho) {
+    if (tamanho == 0) return;
+
+    if (linha < TAM && coluna >= 0) {
+        tabuleiro[linha][coluna] = NAVIO;
+        posicionarNavioDiagonalSecundaria(tabuleiro, linha + 1, coluna -1, tamanho - 1);
+    }
+}
+
 int main() {
     int tabuleiro[TAM][TAM];
-    int linhaHorizontal, colunaHorizontal;
-    int linhaVertical, colunaVertical;
-
-    // Inicializar o tabuleiro
     inicializarTabuleiro(tabuleiro, 0, 0);
 
-    // Entrada do usuário para os navios
-    printf("Digite as coordenadas do navio HORIZONTAL (linha coluna): ");
-    scanf("%d %d", &linhaHorizontal, &colunaHorizontal);
+    // Dois navios horizontais/verticais
+    posicionarNavioHorizontal(tabuleiro, 0, 0, TAM_NAVIO); // Linha 0, col 0
+    posicionarNavioVertical(tabuleiro, 2, 5, TAM_NAVIO); //linha 2, col 5
 
-    printf("Digite as coordenadas do navio VERTICAL (linha coluna): ");
-    scanf("%d %d", &linhaVertical, &colunaVertical);
+    // Dois navios diagonais
+    posicionarNavioDiagonalPrincipal(tabuleiro, 4, 0, TAM_NAVIO); // ↘
+    posicionarNavioDiagonalSecundaria(tabuleiro, 1, 9, TAM_NAVIO); // ↙
 
-    // Validação simples para não ultrapassar os limites
-    if (colunaHorizontal + TAM_NAVIO <= TAM) {
-        posicionarNavioHorizontal(tabuleiro, linhaHorizontal, colunaHorizontal, TAM_NAVIO);
-    } else {
-        printf("Coordenadas invalidas para o navio horizontal!\n");
-    }
-
-    if (linhaVertical + TAM_NAVIO <= TAM) {
-        posicionarNavioVertical(tabuleiro, linhaVertical, colunaVertical, TAM_NAVIO);
-    } else {
-        printf("Coordenadas invalidas para o navio vertical!\n");
-    }
-    
-    // Exibir o tabuleiro
-    printf("\nTabuleiro de Batalha Naval:\n\n");
+    printf("\nTabuleiro 10x10 com navios:\n\n");
     exibirTabuleiro(tabuleiro, 0, 0);
 
     return 0;
 }
+
+
     
